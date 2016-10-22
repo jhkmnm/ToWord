fd7c5b2d2ac888c6fc8a4e079c7fb68e0535b8d5
jhkmnm <jhkmnm_123@163.com>
2016-10-22 18:50:03 +0800

    增加附件窗体

---
+++ ToWord/FormWordsAppendix.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace ToWord
+{
+    partial class FormWordsAppendix
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.btnTzhenwen = new System.Windows.Forms.Button();
+            this.btnBiaoti4 = new System.Windows.Forms.Button();
+            this.btnBiaoti3 = new System.Windows.Forms.Button();
+            this.btnBiaoti2 = new System.Windows.Forms.Button();
+            this.btnBiaoti1 = new System.Windows.Forms.Button();
+            this.txtZhenwen = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTimu = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnAppendix = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label8
+            // 
+            this.label8.AutoEllipsis = true;
+            this.label8.ForeColor = System.Drawing.Color.Red;
+            this.label8.Location = new System.Drawing.Point(14, 636);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(210, 21);
+            this.label8.TabIndex = 47;
+            this.label8.Text = "3. 删除: 选中节点->删除按钮->确定";
+            // 
+            // label7
+            // 
+            this.label7.AutoEllipsis = true;
+            this.label7.ForeColor = System.Drawing.Color.Red;
+            this.label7.Location = new System.Drawing.Point(14, 604);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(210, 32);
+            this.label7.TabIndex = 46;
+            this.label7.Text = "2. 修改: 选中节点->输入框中修改文字->确定";
+            // 
+            // label6
+            // 
+            this.label6.AutoEllipsis = true;
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(14, 562);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(210, 43);
+            this.label6.TabIndex = 45;
+            this.label6.Text = "1. 添加: 选中节点->点击标题或正文按钮->输入框中输入文字->确定，不选节点将会在根节点下添加";
+            // 
+            // btnDel
+            // 
+            this.btnDel.Location = new System.Drawing.Point(137, 191);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(75, 23);
+            this.btnDel.TabIndex = 44;
+            this.btnDel.Text = "删除";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // label5
+            // 
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(570, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(240, 28);
+            this.label5.TabIndex = 43;
+            this.label5.Text = "1. 【保存】按钮会保存当前整个界面的内容2. 【确定】按钮保存左边的内容";
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(32, 191);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 42;
+            this.btnOK.Text = "确定";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // txtContent
+            // 
+            this.txtContent.Location = new System.Drawing.Point(12, 234);
+            this.txtContent.Multiline = true;
+            this.txtContent.Name = "txtContent";
+            this.txtContent.Size = new System.Drawing.Size(212, 317);
+            this.txtContent.TabIndex = 41;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(784, 120);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 40;
+            this.button7.Text = "保存";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.CheckBoxes = true;
+            this.treeView1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.treeView1.HideSelection = false;
+            this.treeView1.Location = new System.Drawing.Point(235, 164);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(660, 584);
+            this.treeView1.TabIndex = 39;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // btnTzhenwen
+            // 
+            this.btnTzhenwen.Location = new System.Drawing.Point(513, 120);
+            this.btnTzhenwen.Name = "btnTzhenwen";
+            this.btnTzhenwen.Size = new System.Drawing.Size(75, 23);
+            this.btnTzhenwen.TabIndex = 38;
+            this.btnTzhenwen.Text = "正文";
+            this.btnTzhenwen.UseVisualStyleBackColor = true;
+            this.btnTzhenwen.Click += new System.EventHandler(this.btnTzhenwen_Click);
+            // 
+            // btnBiaoti4
+            // 
+            this.btnBiaoti4.Location = new System.Drawing.Point(402, 120);
+            this.btnBiaoti4.Name = "btnBiaoti4";
+            this.btnBiaoti4.Size = new System.Drawing.Size(75, 23);
+            this.btnBiaoti4.TabIndex = 37;
+            this.btnBiaoti4.Text = "四级标题";
+            this.btnBiaoti4.UseVisualStyleBackColor = true;
+            this.btnBiaoti4.Click += new System.EventHandler(this.btnBiaoti4_Click);
+            // 
+            // btnBiaoti3
+            // 
+            this.btnBiaoti3.Location = new System.Drawing.Point(288, 120);
+            this.btnBiaoti3.Name = "btnBiaoti3";
+            this.btnBiaoti3.Size = new System.Drawing.Size(75, 23);
+            this.btnBiaoti3.TabIndex = 36;
+            this.btnBiaoti3.Text = "三级标题";
+            this.btnBiaoti3.UseVisualStyleBackColor = true;
+            this.btnBiaoti3.Click += new System.EventHandler(this.btnBiaoti3_Click);
+            // 
+            // btnBiaoti2
+            // 
+            this.btnBiaoti2.Location = new System.Drawing.Point(181, 120);
+            this.btnBiaoti2.Name = "btnBiaoti2";
+            this.btnBiaoti2.Size = new System.Drawing.Size(75, 23);
+            this.btnBiaoti2.TabIndex = 35;
+            this.btnBiaoti2.Text = "二级标题";
+            this.btnBiaoti2.UseVisualStyleBackColor = true;
+            this.btnBiaoti2.Click += new System.EventHandler(this.btnBiaoti2_Click);
+            // 
+            // btnBiaoti1
+            // 
+            this.btnBiaoti1.Location = new System.Drawing.Point(74, 120);
+            this.btnBiaoti1.Name = "btnBiaoti1";
+            this.btnBiaoti1.Size = new System.Drawing.Size(75, 23);
+            this.btnBiaoti1.TabIndex = 34;
+            this.btnBiaoti1.Text = "一级标题";
+            this.btnBiaoti1.UseVisualStyleBackColor = true;
+            this.btnBiaoti1.Click += new System.EventHandler(this.btnBiaoti1_Click);
+            // 
+            // txtZhenwen
+            // 
+            this.txtZhenwen.Location = new System.Drawing.Point(49, 60);
+            this.txtZhenwen.Multiline = true;
+            this.txtZhenwen.Name = "txtZhenwen";
+            this.txtZhenwen.Size = new System.Drawing.Size(846, 42);
+            this.txtZhenwen.TabIndex = 33;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 63);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 12);
+            this.label4.TabIndex = 32;
+            this.label4.Text = "正文:";
+            // 
+            // txtTimu
+            // 
+            this.txtTimu.Location = new System.Drawing.Point(49, 12);
+            this.txtTimu.Multiline = true;
+            this.txtTimu.Name = "txtTimu";
+            this.txtTimu.Size = new System.Drawing.Size(443, 42);
+            this.txtTimu.TabIndex = 30;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 12);
+            this.label3.TabIndex = 29;
+            this.label3.Text = "题目:";
+            // 
+            // btnAppendix
+            // 
+            this.btnAppendix.Location = new System.Drawing.Point(628, 120);
+            this.btnAppendix.Name = "btnAppendix";
+            this.btnAppendix.Size = new System.Drawing.Size(75, 23);
+            this.btnAppendix.TabIndex = 48;
+            this.btnAppendix.Text = "附件";
+            this.btnAppendix.UseVisualStyleBackColor = true;
+            this.btnAppendix.Click += new System.EventHandler(this.btnAppendix_Click);
+            // 
+            // FormWordsAppendix
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(910, 754);
+            this.Controls.Add(this.btnAppendix);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtContent);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.btnTzhenwen);
+            this.Controls.Add(this.btnBiaoti4);
+            this.Controls.Add(this.btnBiaoti3);
+            this.Controls.Add(this.btnBiaoti2);
+            this.Controls.Add(this.btnBiaoti1);
+            this.Controls.Add(this.txtZhenwen);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtTimu);
+            this.Controls.Add(this.label3);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FormWordsAppendix";
+            this.Text = "文字附件";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.TextBox txtContent;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button btnTzhenwen;
+        private System.Windows.Forms.Button btnBiaoti4;
+        private System.Windows.Forms.Button btnBiaoti3;
+        private System.Windows.Forms.Button btnBiaoti2;
+        private System.Windows.Forms.Button btnBiaoti1;
+        private System.Windows.Forms.TextBox txtZhenwen;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtTimu;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnAppendix;
+    }
+}
---
+++ ToWord/FormWordsAppendix.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SRDFAA.Fram.Utilities;
+
+namespace ToWord
+{
+    public partial class FormWordsAppendix : Form
+    {
+        private WordsAppendix _appendix;
+        private Title _title;
+        private string[] stitle = { "正文", "一级标题", "二级标题", "三级标题", "四级标题" };
+        private int index;
+        private bool isadd;
+        private int id;
+        private TreeNode selectedNode;
+        public WordsAppendix Appendix { get { return _appendix; } }
+
+        public FormWordsAppendix(WordsAppendix appendix)
+        {
+            InitializeComponent();
+
+            _appendix = appendix;
+        }
+
+        private void Init()
+        {
+            if (_appendix == null) return;
+
+            txtTimu.Text = _appendix.Timu.ConContent;
+            txtZhenwen.Text = _appendix.TZhenwen.ConContent;
+
+            if (_appendix.Zhenwen != null)
+            {
+                InitTree(null);
+                _appendix.Zhenwen.ForEach(f => id = id == 0 ? f.ID : (f.ID > id ? f.ID : id));
+            }
+        }
+
+        private void InitTree(TreeNode node)
+        {
+            List<Title> nodeSource;
+
+            if (node == null)
+            {
+                nodeSource = _appendix.Zhenwen.FindAll(a => a.Parent == 0);
+            }
+            else
+            {
+                nodeSource = _appendix.Zhenwen.FindAll(a => a.Parent == ((Title)node.Tag).ID);
+            }
+
+            foreach (Title t in nodeSource)
+            {
+                TreeNode no = new TreeNode(t.TContent.ConContent) { Tag = t };
+
+                if (node == null)
+                {
+                    treeView1.Nodes.Add(no);
+                }
+                else
+                {
+                    node.Nodes.Add(no);
+                }
+
+                InitTree(no);
+            }
+        }
+
+        private void GetTree(TreeNodeCollection node)
+        {
+            foreach (TreeNode n in node)
+            {
+                if (_appendix.Zhenwen == null)
+                {
+                    _appendix.Zhenwen = new List<Title>();
+                }
+
+                _appendix.Zhenwen.Add((Title)n.Tag);
+                GetTree(n.Nodes);
+            }
+        }
+
+        private void SaveAppendix()
+        {            
+
+            if (_appendix.Timu == null)
+                _appendix.Timu = new Content() { ConType = "题目" };
+
+            if (_appendix.TZhenwen == null)
+                _appendix.TZhenwen = new Content() { ConType = "正文" };
+                        
+            _appendix.Timu.ConContent = txtTimu.Text;
+            _appendix.TZhenwen.ConContent = txtZhenwen.Text;
+            GetTree(treeView1.Nodes);
+        }
+
+        private void AddTreeNode()
+        {
+            TreeNode n = new TreeNode() { Tag = _title, Text = _title.TContent.ConContent };
+            if (treeView1.Nodes == null)
+            {
+                treeView1.Nodes.Add(n);
+            }
+            else if (treeView1.SelectedNode != null)
+            {
+                treeView1.SelectedNode.Nodes.Add(n);
+            }
+            else
+            {
+                treeView1.Nodes.Add(n);
+            }
+            treeView1.ExpandAll();
+        }
+
+        private void DeleteTreeNode()
+        {
+            var oldN = treeView1.SelectedNode;
+            foreach (TreeNode cn in oldN.Nodes)
+            {
+                ((Title)cn.Tag).Parent = ((Title)cn.Tag).Parent;
+            }
+            treeView1.Nodes.Remove(oldN);
+        }
+
+        private void UpTreeNode()
+        {
+            treeView1.SelectedNode.Tag = _title;
+            treeView1.SelectedNode.Text = _title.TContent.ConContent;
+        }
+
+        private bool AddCheck()
+        {
+            DataVerifier dv = new DataVerifier();
+            dv.Check(selectedNode == null && index > 1, "请按照 一级标题->二级标题->三级标题->四级标题 的顺序添加!");
+            if (selectedNode != null)
+            {
+                int pindex = stitle.ToList().FindIndex(a => ((Title)selectedNode.Tag).TContent.ConType == a);
+                dv.CheckIfBeforePass(pindex == 0 && index != 0, "请非正文后面添加下级标题!");
+                dv.CheckIfBeforePass(index != 0 && pindex != 0 && index != pindex + 1, "请按照 一级标题->二级标题->三级标题->四级标题 的顺序添加!");
+            }
+
+            dv.ShowMsgIfFailed();
+            return dv.Pass;
+        }
+
+        private bool DelCheck()
+        {
+            DataVerifier dv = new DataVerifier();
+
+            dv.Check(selectedNode == null, "请选择要删除的节点");
+            if (selectedNode != null)
+            {
+                if (selectedNode.Nodes.Count > 0)
+                {
+                    var child = selectedNode.Nodes[0];
+                    int sindex = stitle.ToList().FindIndex(a => ((Title)selectedNode.Tag).TContent.ConType == a);
+                    if (child != null)
+                    {
+                        int cindex = stitle.ToList().FindIndex(a => ((Title)child.Tag).TContent.ConType == a);
+                        dv.CheckIfBeforePass(sindex > 0 && cindex > 0, "请按照 一级标题->二级标题->三级标题->四级标题 的顺序");
+                    }
+                }
+            }
+
+            dv.ShowMsgIfFailed();
+            return dv.Pass;
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            _title = (Title)e.Node.Tag;
+            selectedNode = e.Node;
+            isadd = false;
+            txtContent.Text = _title.TContent.ConContent;
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            if (DelCheck())
+            {
+                DeleteTreeNode();
+                selectedNode = null;
+            }
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            if(!string.IsNullOrWhiteSpace(txtContent.Text))
+                _title.TContent.ConContent = txtContent.Text;
+
+            if (isadd)
+            {
+                if (AddCheck())
+                {
+                    AddTreeNode();
+                }
+            }
+            else
+            {
+                UpTreeNode();
+            }
+        }
+
+        private void btnBiaoti1_Click(object sender, EventArgs e)
+        {
+            isadd = true;
+            int _id = 1;
+            if (_title != null)
+            {
+                _id = ++id;
+            }
+            index = 1;
+            SetTitle(_id);
+        }
+
+        private void SetTitle(int id)
+        {
+            int parent = 0;
+            if (index > 1)
+                parent = _title.ID;
+
+            _title = new Title() { ID = id, TContent = new Content() { ConType = stitle[index] }, Parent = parent };
+            txtContent.Text = "";
+            txtContent.Focus();
+        }
+
+        private void btnBiaoti2_Click(object sender, EventArgs e)
+        {
+            isadd = true;
+            index = 2;
+            SetTitle(++id);
+        }
+
+        private void btnBiaoti3_Click(object sender, EventArgs e)
+        {
+            isadd = true;
+            index = 3;
+            SetTitle(++id);
+        }
+
+        private void btnBiaoti4_Click(object sender, EventArgs e)
+        {
+            isadd = true;
+            index = 4;
+            SetTitle(++id);
+        }
+
+        private void btnTzhenwen_Click(object sender, EventArgs e)
+        {
+            isadd = true;
+            index = 0;
+            SetTitle(++id);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            SaveAppendix();
+        }
+
+        private void FormNotice_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+        }
+
+        private void btnAppendix_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog file = new OpenFileDialog())
+            {
+                if(file.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    _title = new Title() { ID = ++id, TContent = new Content() { ConType = "附件中附件" }, Parent = 0 };
+                    _title.TContent.ConContent = file.FileName;
+
+                    if (selectedNode == null)
+                    {
+                        TreeNode n = new TreeNode() { Text = file.FileName, Tag = _title };
+                        treeView1.Nodes.Add(n);
+                    }
+                    else
+                    {
+                        treeView1.SelectedNode.Tag = _title;
+                        treeView1.SelectedNode.Text = _title.TContent.ConContent;
+                    }
+                }
+            }
+        }
+    }
+}
